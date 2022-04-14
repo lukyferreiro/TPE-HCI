f51874b2962fcbc17200b5529977af9f386ff891
Agustina Ortu <aortu@itba.edu.ar>
2022-04-14 15:07:46 -0300

    Creo el webStorm

---
+++ alma_tp/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="light-blue lighten-4"
+      height="120"
+
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="alma logo"
+          src="@/assets/alma_logo.png"
+          width="76"
+        />
+        <p>ALMA</p>
+
+      </div>
+
+      <v-spacer></v-spacer>
+      <v-btn rounded >
+        <v-icon>mdi-help</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+p {color: black ; font-size: 80px ; margin-left: 10px ; margin-top: 30px}
+</style>
---
+++ alma_tp/src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Alma</h1>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({}),
+  }
+</script>
