072ca1566b3516935c5dd541947ae5ae99914d02
Agustina Ortu <aortu@itba.edu.ar>
2022-04-28 21:10:05 -0300

    ready for work !

--- alma_tp/src/App.vue
+++ alma_tp/src/App.vue
@@ -7,34 +7,20 @@
     <router-view/>
 
     <v-app>
-      <v-app-bar
-          app
-          color="light-blue lighten-4"
-          height="120"
-
-      >
-        <div class="d-flex align-center">
-          <v-img
-              alt="alma logo"
-              src="@/assets/alma_logo.png"
-              width="76"
-          />
-          <p>ALMA</p>
-
-        </div>
-
-        <v-spacer></v-spacer>
-        <v-btn rounded >
-          <v-icon>mdi-help</v-icon>
-        </v-btn>
-      </v-app-bar>
-
+      <TopBar/>
       <v-main>
         <HelloWorld/>
       </v-main>
     </v-app>
   </div>
 </template>
+
+<script>
+import TopBar from "@/components/TopBar";
+export default {
+  components: {TopBar},
+}
+</script>
 
 <style lang="scss">
 #app {
---
+++ alma_tp/src/components/TopBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <v-app-bar
+        app
+        color="light-blue lighten-4"
+        height="120"
+
+    >
+      <div class="d-flex align-center">
+        <v-img
+            alt="alma logo"
+            src="@/assets/alma_logo.png"
+            width="76"
+        />
+        <p>ALMA</p>
+
+      </div>
+
+      <v-spacer></v-spacer>
+      <v-btn rounded >
+        <v-icon>mdi-help</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopBar"
+}
+</script>
+
+<style scoped>
+
+</style>
