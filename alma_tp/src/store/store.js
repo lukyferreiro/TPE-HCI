005a74d912671ece6c8f40836041764ad86d7202
Agustina Ortu <aortu@itba.edu.ar>
2022-05-03 19:29:01 -0300

    agregado de store.js para devices

---
+++ alma_tp/src/store/store.js
@@ -0,0 +1,29 @@
+export default {
+    destinations: [
+        {
+            name: "Parlante",
+            image: "parlante.jpg",
+            id: 1
+        },
+        {
+            name: "Puerta",
+            image: "puerta.jpg",
+            id: 2
+        },
+        {
+            name: "Lampara",
+            image: "lampara.jpg",
+            id: 3
+        },
+        {
+            name: "Heladera",
+            image: "heladera.jpg",
+            id: 4
+        },
+        {
+            name: "Aire Acondicionado",
+            image: "aire_acondicionado.jpg",
+            id: 5,
+        }
+    ]
+};
