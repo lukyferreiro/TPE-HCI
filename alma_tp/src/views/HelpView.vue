7281415ddd8c5cb89667e07a2fe8766e4a9ad29e
lukyferreiro <lferreiro@itba.edu.ar>
2022-05-16 13:57:44 -0300

    Mejorando HomeView

--- alma_tp/src/views/HelpView.vue
+++ alma_tp/src/views/HelpView.vue
@@ -1,21 +1,20 @@
 <template>
     <div class="help ml-15 mr-15">
-        <h2>Habitaciones</h2>
+        <h1>Habitaciones</h1>
         <h3>Agregar habitaciones</h3>
-        <p>Ir a la pestaña de habitaciones y clicker en el boton "Agregar habitación" que se encuentra
-          en la esquina inferior derecha de la sección e ingresar el nombre deseado.</p>
+        <p>En la pestaña habitaciones, clicker el boton "Agregar habitación" en la esquina inferior derecha
+          e ingresar el nombre deseado.</p>
         <h3>Personalizar habitaciones.</h3>
-        <p>El boton de configuracion de cada habitacion cuenta con las opciones de: editar el nombre de la habitacion,
-          cambiar su color y poder eliminar la habitación.</p>
+        <p>Para cada habitación se puede editar su respectivo nombre, cambiar su color de visualización y/o eliminarla.</p>
         <h3>Agregar dispositivos</h3>
-        <p>Si contamos con una habitacion creada, podremos hacer click en el boton "Agregar dispositivo", donde
-          se podra observar un listado de todos los dispositivos soportados por ALMA.</p>
+        <p>Con una habitación ya creada, se podra clicker el boton "Agregar dispositivo", elegir el dispositivo a agregar
+          y luego configurarlo.</p>
 
-        <h2>Rutinas</h2>
+        <h1>Rutinas</h1>
         <h3>Agregar rutinas</h3>
-        <p> Para poder crear una nueva rutina, se debe contar con habitaciones y dispositivos previamente creados y vinculados.
-          Ir a la pestaña de habitaciones y clicker en el boton "Agregar habitación" que se encuentra en la esquina inferior derecha
-          de la sección. Alli se solicitara un nombre para la rutina, un horario de ejecución, los dias que desean ejecutarse,
+        <p> Previamente se debe contar con al menos una habitación creada y dispositivos vinculados a ella.
+          En la pestaña rutinas, clicker el boton "Agregar rutina" en la esquina inferior derecha de la sección.
+          Alli se solicitara un nombre para la rutina, un horario de ejecución, los dias que desean ejecutarse,
           y seleccionar las habitaciones/dispositivos que manejaran la rutina.</p>
     </div>
 </template>
--- alma_tp/src/views/HomeView.vue
+++ alma_tp/src/views/HomeView.vue
@@ -1,23 +1,55 @@
 <template>
     <div class="home">
         <div class="text">
-            <h1>Bienvenidos a ALMA :D</h1>
+            <h1>Bienvenido/a a ALMA</h1>
+            <p>
+                <v-icon color="black" size="30px" class="pb-1 mr-2">mdi-devices</v-icon>
+                Controla tus dispositivos remotamente.
+            </p>
+            <p>
+                <v-icon color="black" size="30px" class="pb-1 mr-2">mdi-web</v-icon>
+                Automatiza con facilidad.
+            </p>
+            <h2> Comienza agregando tu primera habitación:</h2>
+            <div class="habsButton">
+                <v-btn color="secondary"
+                       @click="updateRooms()"
+                       :to="{name:'RoomView'}"
+                       x-large
+                       rounded elevation="5"
+                       retain-focus-on-click>
+                    Habitaciones
+                </v-btn>
+            </div>
         </div>
         <div class="image">
             <v-img alt="Imagen de fondo"
-                   :src="require(`@/assets/welcomeHome.png`)"
-                   class="mx-auto"
-                   max-width="80%"
-                   max-height="80%"
-            />
+                   :src="image"/>
         </div>
     </div>
 </template>
 
 <script>
+import {mapActions} from "vuex";
+
 export default {
-  name: "HomeView"
+    name: "HomeView",
+    data() {
+      return {
+        image: require(`@/assets/welcomeHome.png`),
+      }
+    },
+    methods: {
+      ...mapActions("room", {
+        $getAllRooms: "getAll",
+      }),
+
+      async updateRooms() {
+        await this.$getAllRooms();
+      },
+    }
 }
+
 </script>
 
 <style scoped>
@@ -30,11 +62,38 @@ export default {
   }
 
   .text{
+    font-weight: bold;
+  }
 
+  h1{
+    font-size: 50px;
+    margin-bottom: 15px;
+  }
+
+  p, h2{
+    padding-top: 20px;
+    padding-bottom: 20px;
+  }
+
+  p{
+    font-size: 25px;
+    padding-left: 15px;
+    margin: 0;
+  }
+
+  h2{
+    font-size: 30px;
   }
 
   .image{
-    margin-top: 30px;
+    height: 78vh;
+    max-width: 40%;
+  }
+
+  .habsButton, .image, h1, p, h2{
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 
 </style>
