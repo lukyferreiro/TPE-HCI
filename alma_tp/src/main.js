f3bc41e07132201aa253f4d32ec95b47f5d7ef24
Mechi Baron <mbaron@itba.edu.ar>
2022-05-18 22:42:40 -0300

    resfresh

--- alma_tp/src/main.js
+++ alma_tp/src/main.js
@@ -10,5 +10,19 @@ new Vue({
   vuetify,
   router,
   store,
+  el: '#app',
+  components: { App },
+  template: '<App/>',
+  created() {
+    if (performance.navigation.type === 1) {
+      if(this.$route.path === '/') {
+        if (confirm('¿Recargar el sitio?. Los cambios no serán guardados.')) {
+          this.$router.push({path: '/'})
+        } else {
+          console.log('reload page now');
+        }
+      }
+    }
+  },
   render: h => h(App)
 }).$mount('#app')
